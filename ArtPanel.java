df5794cc9a38b3dc27407ccd6aa6e3d762246355
jawauntb <Juan@Jawauns-MacBook-Pro.local>
2018-04-09 13:18:14 -0400

    this is the panel which implements the canvas method calls

---
+++ ArtPanel.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+//  This file describes the construction and behavior of the main
+//  panel
+//                   J Brown 2016 for Aarathi Prasad-Amherst College
+
+
+public class Lab3Panel extends JPanel implements ActionListener {
+
+    private Lab3Canvas myCanvas;
+    private JButton colorButton;
+
+    Lab3Panel() {
+
+	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+	// We create various graphical objects and add them
+	// to the frame
+
+	JLabel msg1 = new JLabel ("Clicking on the button " +
+				"changes the color of the object");
+	add(msg1);
+	msg1.setAlignmentX(0.5f);
+
+	add(Box.createVerticalStrut(20));
+
+	colorButton = new JButton("Change color");
+	colorButton.addActionListener(this);
+	add(colorButton);
+	colorButton.setAlignmentX(0.5f);
+	
+	add(Box.createVerticalStrut(20));
+
+	myCanvas = new Lab3Canvas();
+	add(myCanvas);
+	myCanvas.setAlignmentX(0.5f);
+
+	setBorder (new EmptyBorder(50, 50, 50, 50));
+    }
+
+    public void actionPerformed (ActionEvent e) {
+
+	// Here we handle button clicks, by invoking methods for our object
+
+	myCanvas.changeColor();
+	myCanvas.repaint();
+    }
+}
